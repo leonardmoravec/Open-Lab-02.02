a848e336374f78f10dcf5bd375adb8d6b74fe59e
leonardmoravec <leonardmoravec154@gmai.lcom>
2021-10-19 21:21:26 +0200

    I implemented function that converts input numbers to 2 decimal place

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            int i = 0;
+            string[] To_Be_Outputed = new string[numbers.Length];
+            foreach (double item in numbers)
+        
+        {
+            double rounded = Math.Round(item, 2);
+            string Converted_To_String = rounded.ToString();
+            string Replaced_Comma = Converted_To_String.Replace(',', '.');
+            To_Be_Outputed[i] = Replaced_Comma;
+                   i = i + 1;
         }
+    Console.Write(string.Join("", To_Be_Outputed)); 
     }
 }
